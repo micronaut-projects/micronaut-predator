49c17a97f36b19a08ec1cee744476a1eb3c5f574
radovanradic <radicr@gmail.com>
2024-12-10 18:00:03 +0100

    Fixed postgres repository dialect

--- data-r2dbc/src/test/java/io/micronaut/data/r2dbc/postgres/PostgresExampleEntityRepository.java
+++ data-r2dbc/src/test/java/io/micronaut/data/r2dbc/postgres/PostgresExampleEntityRepository.java
@@ -4,6 +4,6 @@ import io.micronaut.data.model.query.builder.sql.Dialect;
 import io.micronaut.data.r2dbc.annotation.R2dbcRepository;
 import io.micronaut.data.tck.repositories.ExampleEntityRepository;
 
-@R2dbcRepository(dialect = Dialect.H2)
+@R2dbcRepository(dialect = Dialect.POSTGRES)
 public interface PostgresExampleEntityRepository extends ExampleEntityRepository {
 }
